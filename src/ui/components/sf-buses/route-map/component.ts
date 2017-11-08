7c723fadf62940222a4eca23ef3ff748b5183daf
Eric Artzt <eric.artzt@avalara.com>
2017-11-08 03:12:29 -0800

    Applying markers to map Rework the data loading

--- src/ui/components/sf-buses/route-map/component.ts
+++ src/ui/components/sf-buses/route-map/component.ts
@@ -1,8 +1,12 @@
-import Component from '@glimmer/component';
+import Component, { tracked } from '@glimmer/component';
+import { fetchRouteLocations } from '../../../../utils/fetch-routes';
 
 const apiKey = 'AIzaSyA7xlBaE1zcBwPAKJEnYUO0e2FOOPGAsb0';
 
 export default class RouteMap extends Component {
+  map : any;
+  markers : any;
+
   didInsertElement() {
     this.loadGoogleMaps();
   }
@@ -17,16 +21,66 @@ export default class RouteMap extends Component {
   }
 
   onMapInit() {
-    console.log(google.maps);
-    var uluru = {lat: -25.363, lng: 131.044};
-    var map = new google.maps.Map(document.getElementById('gmap-canvas'), {
-      zoom: 4,
-      center: uluru
+    const center = {
+      lat: 37.768528,
+      lng: -122.290307,
+    };
+
+    this.map = new google.maps.Map(document.getElementById('gmap-canvas'), {
+      zoom: 10,
+      center,
     });
-    var marker = new google.maps.Marker({
-      position: uluru,
-      map: map
+
+    this.getBusesForRoutes()
+      .then((busesForRoutes) => {
+
+        console.log('busesForRoutes', busesForRoutes);
+        this.drawRoutes(busesForRoutes);
+      });
+  }
+
+  async getBusesForRoutes() {
+    const time = 0;
+
+    const routes = this.args.routes;
+
+    console.log('routes', routes);
+
+    return await Promise.all(routes.map(route => fetchRouteLocations(route, time)));
+  }
+
+  @tracked('args')
+  get argsUpdated() {
+    console.log('args', this.args);
+    return this.args;
+  }
+
+  drawRoutes(routes) {
+    const map = this.map;
+    const markers = this.markers || [];
+
+    // clear any existing markers
+
+    routes.forEach((route) => {
+      const buses = route.buses;
+      if (!buses) {
+        console.log('whoops', route.tag);
+        return;
+      }
+
+      buses.forEach((bus) => {
+        const latLng = new google.maps.LatLng(bus.lat, bus.lon);
+        const marker = new google.maps.Marker({
+          position: latLng,
+          map: map,
+          title: route.tag,
+        });
+
+        markers.push(marker);
+      });
     });
+
+    this.markers = markers;
   }
 };
 
